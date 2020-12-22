0cf7dd9fd5b1940970cb77f66a64cd3663c2699a
Eryk Knesz <eryk.knesz@gmail.com>
2020-12-22 18:42:14 +0100

    change congig and requirements to enable postgresql

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,5 +11,6 @@ class Config:
             'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME")
     ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
