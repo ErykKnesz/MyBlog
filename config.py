ff9f480f3181efa3733e98fae8d15f0f8bb8d2cb
Eryk Knesz <eryk.knesz@gmail.com>
2020-12-22 22:14:34 +0100

    update config

--- config.py
+++ config.py
@@ -6,10 +6,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = (
-            os.environ.get('DATABASE_URL') or
-            'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
-    )
+    DATABASE_URL = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME")
